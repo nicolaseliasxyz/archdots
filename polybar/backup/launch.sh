4bc41c062a42f780a965e501b08e7691be0b8f7c
Nicolas Elias <nicolaselias@pm.me>
2023-10-11 10:14:30 -0300

    polybar

--- polybar/backup/launch.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-killall polybar
-
-polybar main -c ~/.config/polybar/polybar-collection/themes/minimal/config.ini &
-polybar main2 -c ~/.config/polybar/polybar-collection/themes/minimal/config.ini &
